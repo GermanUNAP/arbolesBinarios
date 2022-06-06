8dd8a9370a7bad38d505369f5d51881e12c3403e
germanUNAP <ccus.german.chura@gmail.com>
2022-06-07 02:28:12 +1300

    Se añaden los demas ejercios, ademas eliminar un nodo del arbol

--- arbol_binario_busqueda.cpp
+++ arbol_binario_busqueda.cpp
@@ -1,4 +1,13 @@
-//
+/*
+1-3    Al recorrer el árbol cuantas llanadas recursivas
+4-6    Encontrar el nivel del árbol
+7-9    Encontrar el mayor elemento
+10-12  Encontrar el menor elemento
+13-15  Sumar y Hallar el Promedio
+16-18  Mostrar de mayor a menor
+19-21  Mostrar de menor a mayor
+22-24  Al recorrer el árbol cuantas paradas hay
+*/
 
 #include <iostream>
 #include <conio.h>
@@ -10,14 +19,21 @@ struct Nodo{
 	int dato;
 	Nodo *der;
 	Nodo *izq;
+	Nodo *padre;
 };
 Nodo *arbol = NULL;
 
+//declaring global variables
 int nivelArbol = 0;
+int contRecursivo = 0;
+float sum = 0;
+float  prompt = 0;
+int stop;
 
+//declaring functions
 void menu();
-Nodo *crearNodo(int n);
-void insertarNodo(Nodo *&, int);
+Nodo *crearNodo(int n, Nodo *);
+void insertarNodo(Nodo *&, int, Nodo *);
 void mostrarArbol(Nodo *, int);
 bool busqueda(Nodo *, int);
 void preOrden(Nodo *);
@@ -28,7 +44,15 @@ void descendente(Nodo *);
 int mayorValor(Nodo *);
 int menorValor(Nodo *);
 int nivel(Nodo *, int);
-
+int llamadasRecursivas(Nodo *);
+int suma(Nodo *);
+float promedio(Nodo *);
+int paradas(Nodo *);
+void eliminarNodo(Nodo *, int);
+void eliminar(Nodo *arbol);
+Nodo *minimo(Nodo *);
+void reemplazar(Nodo *, Nodo *);
+void destruirNodo (Nodo *);
 
 int main(){
 	menu();
@@ -38,28 +62,37 @@ int main(){
 
 //funcion de menú
 void menu(){
+	system("color a");
 	int dato, opcion, cont = 0;
 	do{
 		nivelArbol = 0;
+		contRecursivo = 0;
+		sum = 0; 
+		prompt = 0;
+		stop = 0;
 		cout<<"\tMENU: "<<endl;
-		cout<<"1. Insertar un nuevo nodo "<<endl;
-		cout<<"2. Mostrar el arbol completo"<<endl;
-		cout<<"3. Buscar un elemento en el arbol"<<endl;
-		cout<<"4. Recorrido Pre Orden "<<endl;
-		cout<<"5. Reorrido en In Orden"<<endl;
-		cout<<"6. Recorrido en Post Orden"<<endl;
-		cout<<"7. Mostrar el arbol de manera ascendente"<<endl;
-		cout<<"8. Mostrar el arbol de manera descendente"<<endl;
-		cout<<"9. Buscar el mayor valor del arbol"<<endl;
-		cout<<"10. Buscar el menor valor del arbol"<<endl;
-		cout<<"11. Encontrar el nivel del nodo de una rbol"<<endl;	
-		cout<<"12. Salir"<<endl;
+		cout<<"1. Insertar un NUEVO NODO "<<endl;
+		cout<<"2. MOSTRAR el arbol completo"<<endl;
+		cout<<"3. BUSCAR un elemento en el arbol"<<endl;
+		cout<<"4. Recorrido PRE ORDEN "<<endl;
+		cout<<"5. Reorrido en IN ORDEN"<<endl;
+		cout<<"6. Recorrido en POST ORDEN"<<endl;
+		cout<<"7. Mostrar el arbol de manera ASCENDENTE"<<endl;
+		cout<<"8. Mostrar el arbol de manera DESCENDENTE"<<endl;
+		cout<<"9. Buscar el MAYOR valor del arbol"<<endl;
+		cout<<"10. Buscar el MENOR valor del arbol"<<endl;
+		cout<<"11. Encontrar el NIVEL del nodo de un arbol"<<endl;
+		cout<<"12. Al recorrer el arbol cuantas LLAMADAS RECURSIVAS"<<endl;
+		cout<<"13. SUMAR y halllar el PROMEDIO"<<endl;
+		cout<<"14. Al recorrer el arbol cuantas PARADAS hay"<<endl;
+		cout<<"15. ELIMINAR un nodo de un arbol"<<endl;
+		cout<<"18. Salir"<<endl;
 		cin>>opcion;
 		switch(opcion){
 			case 1: 
 				cout<<endl<<"Digite un numero: ";
 				cin>>dato;
-				insertarNodo(arbol, dato);
+				insertarNodo(arbol, dato, NULL);
 				break;
 			case 2: 
 				cout<<endl<<"Mostrando el arbol completo"<<endl;
@@ -125,39 +158,62 @@ void menu(){
 				cin>>dato;
 				cout<<"El nivel del nodo es: "<<nivel(arbol, dato)<<endl;
 				system("pause");
-				break;	
+				break;
+			case 12:
+				cout<<"Encontrando las llamadas recursivas que se hace al arbol:"<<endl;
+				cout<<"Es un total del: "<<llamadasRecursivas(arbol)<<endl;	
+				system("pause");
+				break;
+			case 13:
+				cout<<"La suma del arbol es: "<<suma(arbol)<<endl;
+				cout<<"El promedio del arbol es: "<<promedio(arbol)<<endl;
+				system("pause");
+				break;
+			case 14:
+				cout<<"Las paradas que se realiza en el arbol"<<endl;
+				cout<<"El numero de paradas es: "<<paradas(arbol)<<endl;
+				system("pause");
+				break;
+			case 15:
+				cout<<"Ingrese nodo a eliminar: "<<endl;
+				cin>>dato;
+				eliminarNodo(arbol, dato);
+				cout<<endl;
+				system("pause");
+				break;
 		}
 		system("cls");
 	}
-	while (opcion != 12);
+	while (opcion != 18);
 }
 
 
 //funcion para crear nodo
-Nodo *crearNodo(int n){
+Nodo *crearNodo(int n, Nodo *padre){
 	Nodo *nuevo_nodo = new Nodo();
 	
 	nuevo_nodo->dato = n;
 	nuevo_nodo->der = NULL;
 	nuevo_nodo->izq = NULL;
+	nuevo_nodo->padre = padre;
 	
 	return nuevo_nodo;
 }
 
 //funcion para insertar nodos en el arbol
 
-void insertarNodo(Nodo *&arbol, int n){
+void insertarNodo(Nodo *&arbol, int n, Nodo *padre){
 	if(arbol == NULL){
-		Nodo *nuevo_nodo = crearNodo(n);
+		Nodo *nuevo_nodo = crearNodo(n, padre);
 		arbol = nuevo_nodo;
 	}
 	else{
 		int valorRaiz = arbol->dato;
 		if(n < valorRaiz){
-			insertarNodo(arbol->izq, n);
+			insertarNodo(arbol->izq, n, arbol);
 		}
 		else{
-			insertarNodo(arbol->der, n);
+			insertarNodo(arbol->der, n, arbol);
 		}
 	}
 }
@@ -283,3 +339,126 @@ int nivel(Nodo *arbol, int n){
 	nivelArbol++;
 }
 
+int llamadasRecursivas(Nodo *arbol){
+	if(arbol == NULL){
+		contRecursivo++;
+		return contRecursivo;
+	}
+	else{
+		contRecursivo++;
+		llamadasRecursivas(arbol->izq);
+		llamadasRecursivas(arbol->der);	
+	}
+}
+
+int suma(Nodo *arbol){
+	if(arbol == NULL){
+		return sum;
+	}
+	else{
+		sum += arbol->dato;
+		suma(arbol->izq);
+		suma(arbol->der);
+	}
+}
+
+float promedio(Nodo *arbol){
+	if(arbol == NULL){
+		return sum/prompt;
+	}
+	else{
+		prompt++;
+		promedio(arbol->izq);
+		promedio(arbol->der);
+	}
+}
+
+//funcion para verificar las paradas que se realizan en el arbol
+int paradas(Nodo *arbol){
+	if(arbol == NULL){
+		stop++;
+		return stop;
+	}
+	else{
+		paradas(arbol->izq);
+		paradas(arbol->der);
+	}
+	
+}
+
+//funcion para eliminar un nodo del arbol
+void eliminarNodo(Nodo *arbol, int n){
+	if(arbol == NULL){
+		return ;
+	}
+	else if(n < arbol->dato){
+		eliminarNodo(arbol->izq, n);
+	}
+	else if(n > arbol->dato){
+		eliminarNodo(arbol->der, n);
+	}
+	else if(n == arbol->dato){
+		eliminar(arbol);
+	}
+}
+
+//funcion para hallar el minimo elemento
+Nodo *minimo (Nodo *arbol){
+	if(arbol == NULL){
+		return NULL;
+	}
+	if(arbol->izq){ //si tiene hijo izquierdo
+		minimo(arbol->izq);
+	}
+	else{//si ya no tiene hijo, se retortan el Nodo
+		return arbol;
+	}
+}
+
+//funcion para reemplazar 
+void reemplazar(Nodo *arbol, Nodo *nuevoNodo){
+	if(arbol->padre){
+		//hay que asignarle su hijo del nodo a eliminar
+		if(arbol->dato == arbol->padre->izq->dato){
+			arbol->padre->izq=nuevoNodo;
+		}
+		else if(arbol->dato == arbol->padre->der->dato){
+			arbol->padre->der = nuevoNodo;
+		}
+	}
+	if(nuevoNodo){
+		//se le asigna su nuevo padre
+		nuevoNodo->padre = arbol->padre;
+	}
+}
+
+//funcion para destruir nodo
+
+void destruir(Nodo *nodo){
+	nodo->izq = NULL;
+	nodo->der=NULL;
+	
+	delete nodo;
+}
+
+//funcion para eliminar el nodo encontrado
+void eliminar(Nodo *nodoEliminar){
+	if(nodoEliminar->izq  && nodoEliminar->der ){
+		Nodo *menor = minimo(nodoEliminar->der);
+		nodoEliminar->dato = menor->dato;
+		eliminar(menor);
+	}
+	else if(nodoEliminar->izq){//si solo tiene hijo izquierdo
+		reemplazar(nodoEliminar, nodoEliminar->izq);
+		destruir(nodoEliminar);
+	}
+	else if(nodoEliminar->der){//si tiene solo hijo izquierdo
+		reemplazar(nodoEliminar, nodoEliminar->der);
+		destruir(nodoEliminar);
+	}
+	else{//si no tieje hijos
+		reemplazar(nodoEliminar, NULL);
+		destruir(nodoEliminar);
+	}
+	
+}
